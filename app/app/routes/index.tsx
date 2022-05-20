321b259f2b665e772625447e3ead37e3303883b3
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-05-20 17:44:49 +0200

    first push

---
+++ app/app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+       <h1>بسم الله الرحمن الرحيم</h1>
+      
+    </div>
+  );
+}
